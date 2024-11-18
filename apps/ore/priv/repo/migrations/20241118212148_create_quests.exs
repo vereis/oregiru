803bca38823ea9de9fa5bbf38fd0d7e7d7c6616d
Chris Bailey <me@cbailey.co.uk>
2024-11-18 21:27:15 +0000

    Add quests migration

---
+++ apps/ore/priv/repo/migrations/20241118212148_create_quests.exs
@@ -0,0 +1,20 @@
+defmodule Ore.Repo.Migrations.CreateQuests do
+  use Ecto.Migration
+
+  def change do
+    create table(:quests) do
+      add(:guild_id, references(:guilds, on_delete: :delete_all), null: false)
+      add(:name, :string, null: false)
+      add(:status, :string, null: false)
+      add(:min_level, :integer, default: 0)
+      add(:max_level, :integer, default: 999)
+    end
+
+    create table(:guild_members_quests) do
+      add(:quest_id, references(:quests, on_delete: :delete_all), null: false)
+      add(:member_id, references(:guild_members, on_delete: :delete_all), null: false)
+    end
+
+    create(index(:quests, [:guild_id, :status]))
+  end
+end
